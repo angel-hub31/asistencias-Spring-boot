097436987182ea1e098165334524e2778f17c4ad
angel-hub31 <moralesmales1@hotmail.com>
2026-05-04 15:52:02 -0500

    se crea los constructores, getters y setters en la clase Estudiante

--- src/main/java/com/krakedev/asistencias/entidades/Estudiante.java
+++ src/main/java/com/krakedev/asistencias/entidades/Estudiante.java
@@ -5,7 +5,51 @@ public class Estudiante {
 	private String nombre;
 	private String apellido;
 	
+	//costructor vacio
+	public Estudiante() {
+		super();
+	}
+
+//constructor que recibe parametro parametros
+	public Estudiante(String cedula, String nombre, String apellido) {
+		
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+
+	public String getCedula() {
+		return cedula;
+	}
+
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
 	
 	
 
 }
+
